4f83226e5cf84994588ac99d5877c78a1736ff03
Andreas Müller <schnitzeltony@gmail.com>
2024-10-31 09:32:29 +0100

    xmlconfigreader.h: Remove documentation dust / fix indentation / rework includes
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-xmlconfig/lib/xmlconfigreader.h
+++ zera-xmlconfig/lib/xmlconfigreader.h
@@ -3,107 +3,40 @@
 
 #include "zera-xmlconfig_export.h"
 #include <QObject>
-#include <QXmlStreamReader>
 #include <QXmlStreamWriter>
 #include <QIODevice>
 
 namespace Zera
 {
-  namespace XMLConfig
-  {
+namespace XMLConfig
+{
 
-    class cReaderPrivate;
+class cReaderPrivate;
 
-    /**
-     * @brief The XMLConfigReader class
-     * Reads XSD and XML files as configuration
-     */
-    class ZERA_XMLCONFIG_EXPORT cReader : public QObject
-    {
-      Q_OBJECT
+class ZERA_XMLCONFIG_EXPORT cReader : public QObject
+{
+    Q_OBJECT
+public:
+    explicit cReader(QObject *parent = 0);
+    bool loadSchema(QString filePath);
+    bool loadXMLFile(QString path);
+    bool loadXMLFromString(QString xmlString);
+    QString getValue(QString key);
+    bool setValue(QString key, QString value);
+    QString getXMLConfig();
+signals:
+    void valueChanged(QString key);
+    void finishedParsingXML(bool ok);
 
-    public:
-      /**
-       * @brief Reader default constructor
-       * @param parent
-       */
-      explicit cReader(QObject *parent = 0);
-
-      /**
-       * @brief Load a XSD schema file and initialize the configuration
-       * @param filePath File to load
-       * @return true for success
-       */
-      bool loadSchema(QString filePath);
-      /**
-       * @brief Loads a XML file and sets the configuration if it matches the schema file
-       * @param filePath File to load
-       * @return true for success
-       */
-      bool loadXMLFile(QString path);
-
-      /**
-       * @brief Parses the QString and sets the configuration if the XML data matches the schema file
-       * @param xmlString string to parse
-       * @return true for success
-       */
-      bool loadXMLFromString(QString xmlString);
-
-      /**
-       * @brief Retrieves a value from the configuration map
-       * @param key The map key where the value has been stored
-       * @return QString value
-       */
-      QString getValue(QString key);
-
-      /**
-       * @brief Sets config values
-       * @param key The configuration key in the QHash
-       * @param value The new value
-       *
-       * @return true for success
-       */
-      bool setValue(QString key, QString value);
-
-      /**
-       * @brief Exports the QHash to a XML file format
-       * @return XML code
-       */
-      QString getXMLConfig();
-
-
-    signals:
-      /**
-       * @brief Notifies when values change
-       * @param key The key that changed
-       */
-      void valueChanged(QString key);
-
-      /**
-       * @brief Loading the XML config file is complete
-       */
-      void finishedParsingXML(bool ok);
-
-    protected:
-      /**
-       * @brief Opaque pointer for ABI safety
-       */
-      Zera::XMLConfig::cReaderPrivate *d_ptr;
-
-    private:
-      /**
-       * @brief xml2Config
-       * @param xmlData
-       * @return true for success
-       */
-      bool xml2Config(QIODevice* xmlData);
-
-      void parseLists(QList<QString> oldList, QList<QString> newList, QXmlStreamWriter &writer);
-
-      Q_DISABLE_COPY(cReader)
-      Q_DECLARE_PRIVATE(cReader)
-    };
-  }
+protected:
+    Zera::XMLConfig::cReaderPrivate *d_ptr;
+private:
+    bool xml2Config(QIODevice* xmlData);
+    void parseLists(QList<QString> oldList, QList<QString> newList, QXmlStreamWriter &writer);
+    Q_DISABLE_COPY(cReader)
+    Q_DECLARE_PRIVATE(cReader)
+};
+}
 }
 
 #endif // XMLCONFIGREADER_H
