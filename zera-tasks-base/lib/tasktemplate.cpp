a16156d547887ac9ec1c1e948a7c96b7dbe2f107
Andreas Müller <schnitzeltony@gmail.com>
2025-01-08 23:30:51 +0100

    Introduce TaskTemplate::finishTaskQueued
    We invented tasks as a replacement for state machines and they work fine in
    several places.
    
    Now we learned that we missed important: State machines change state queued but
    tasks - often part of a sequential sequence - are direct connected and transit
    immediately.
    So when replacing state machines we can run into trouble: E.g in a sequential task
    sequence the next task can start before events generated by previous task
    are handled.
    Up to now this has not been a problem but now we try to replace a state machine
    in zera-classes' BaseModule and do not get tests to pass.
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-tasks-base/lib/tasktemplate.cpp
+++ zera-tasks-base/lib/tasktemplate.cpp
@@ -15,6 +15,15 @@ void TaskTemplate::finishTask(bool ok)
     emit sigFinish(ok, m_taskId);
 }
 
+void TaskTemplate::finishTaskQueued(bool ok)
+{
+    QMetaObject::invokeMethod(this,
+                              "sigFinish",
+                              Qt::QueuedConnection,
+                              Q_ARG(bool, ok),
+                              Q_ARG(int, m_taskId));
+}
+
 int TaskTemplate::m_taskIdForNext = 0;
 
 int TaskTemplate::getNextTaskId()
