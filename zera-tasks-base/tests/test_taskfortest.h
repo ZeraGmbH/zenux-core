45831779abdaaa55baabbf1dd9448b1856466179
Andreas Müller <schnitzeltony@gmail.com>
2025-04-10 01:11:26 +0200

    Add test_taskfortest - we found them in the depth of zera-classes
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ zera-tasks-base/tests/test_taskfortest.h
@@ -0,0 +1,21 @@
+#ifndef TEST_TASKFORTEST_H
+#define TEST_TASKFORTEST_H
+
+#include <QObject>
+
+class test_taskfortest : public QObject
+{
+    Q_OBJECT
+private slots:
+    void init();
+    void onePassImmediate();
+    void oneErrImmediate();
+    void onePassDelayed();
+    void oneErrDelayed();
+    void onePassCount();
+    void oneErrCount();
+    void oneDtorCount();
+    void taskId();
+};
+
+#endif // TEST_TASKFORTEST_H
