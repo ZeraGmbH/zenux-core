77a32a86fa3533b81b612bad96a0c5df0b6f0fed
Asmae Kaouissi <a.kaouissi@zera.de>
2026-02-02 09:48:41 +0100

    xmlelemcompareattribs: add list of dtrings to be ignored
    Signed-off-by: Asmae Kaouissi <a.kaouissi@zera.de>

--- testlibs/xml-compare/lib/xmlelemcompareattribs.cpp
+++ testlibs/xml-compare/lib/xmlelemcompareattribs.cpp
@@ -1,4 +1,5 @@
 #include "xmlelemcompareattribs.h"
+#include <QStringList>
 
 XmlAttribCompareFunc XmlElemCompareAttribs::m_defaultAttribCompareFunc =
     [](const QString& attrib1, const QString& attrib2) -> bool
@@ -33,10 +34,13 @@ bool XmlElemCompareAttribs::bothEmpty(const QDomElement &elem1, const QDomElemen
 bool XmlElemCompareAttribs::compareAll(const QDomElement &elem1, const QDomElement &elem2,
                                        XmlAttribCompareFunc attrCompare)
 {
+    QStringList keysToBeIgnored= QStringList() << "transform" << "y";
     auto elem1Attribs = elem1.attributes();
     auto elem2Attribs = elem2.attributes();
     for(int i=0; i<elem1Attribs.count(); ++i) {
         QString attrKey = elem1Attribs.item(i).toAttr().name();
+        if(keysToBeIgnored.contains(attrKey))
+            continue;
         if(!elem2Attribs.contains(attrKey))
             return false;
         QString attrib1 = elem1Attribs.item(i).toAttr().value();
